35685d135c813e8fcf62aa4f779b846f5bbcebd6
alexei4studio-afk <alexei4studio@gmail.com>
2026-04-19 04:53:43 -0700

    fix: explicit relative import for HeroVideo

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import HeroVideo from "@/components/HeroVideo";
+import HeroVideo from "../components/HeroVideo";
 
 // Navbar simplificat pentru stabilitate build
 function Navbar() {
